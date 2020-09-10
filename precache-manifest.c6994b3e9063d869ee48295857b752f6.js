2a76532fca49f7bef269722681edb3d22a067a1c
Mawuvideka <61460954+Mawuvideka@users.noreply.github.com>
2020-09-10 09:38:38 +0000

    Updates

---
+++ precache-manifest.c6994b3e9063d869ee48295857b752f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad47db2b10d2189b9435bc74558afb5f",
+    "url": "/Travel-and-Tours-Reactjs/index.html"
+  },
+  {
+    "revision": "45f6f050b2dea124500e",
+    "url": "/Travel-and-Tours-Reactjs/static/css/main.6a29ae2c.chunk.css"
+  },
+  {
+    "revision": "ba946ca2db1335ab7487",
+    "url": "/Travel-and-Tours-Reactjs/static/js/2.c8f94ae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Travel-and-Tours-Reactjs/static/js/2.c8f94ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f6f050b2dea124500e",
+    "url": "/Travel-and-Tours-Reactjs/static/js/main.093baae7.chunk.js"
+  },
+  {
+    "revision": "7e5f8b09117ae9335d33",
+    "url": "/Travel-and-Tours-Reactjs/static/js/runtime-main.e604da9a.js"
+  },
+  {
+    "revision": "f87f97b6cd83f7d2ef944c23d425585d",
+    "url": "/Travel-and-Tours-Reactjs/static/media/logo.f87f97b6.svg"
+  }
+]);
